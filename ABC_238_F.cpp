4457e09ff4cd57c621a777bb7d84608af09f80e0
gtkn <sks.bumpts@gmail.com>
2022-04-22 23:50:45 +0900

    220422

---
+++ ABC_238_F.cpp
@@ -0,0 +1,93 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+using ll = long long;
+using P = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+
+//using mint = modint1000000007;
+using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+//------------------------------------------------
+
+struct Solver{
+    struct edge{
+        ll to,c;
+        edge(ll to=0, ll c=0):to(to),c(c){}
+    };
+
+    struct abc{
+        ll a,b,c;
+        abc(ll a=0, ll b=0, ll c=0):a(a),b(b),c(c){}
+    };
+
+ 
+ 
+    vec(int) dh = {1,0,-1,0};
+    vec(int) dw = {0,1,0,-1};
+ 
+    void solve(){
+        ll N,K;
+        cin >> N >> K;
+
+        vec(ll) P(N),Q(N);
+        rep(i,N) cin >> P[i];
+        rep(i,N) cin >> Q[i];
+
+        vec(ll) v(N+1);
+        rep(i,N) v[P[i]] = Q[i];
+
+        vvvec(mint) dp(N+10, vvec(mint)(N+10,vec(mint)(N+10)));
+        
+        dp[0][0][N+1] = 1;
+        rep(i,N)rep(j,K+1)rep(k,N+2){
+            dp[i+1][j][min(k,v[i+1])] += dp[i][j][k];
+            if(k>v[i+1]) dp[i+1][j+1][k] += dp[i][j][k];
+        }
+
+        mint ans=0;
+        rep(k,N+2) ans+=dp[N][K][k];
+        cout << ans.val() << endl;
+        
+
+    }
+};
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        Solver solver;
+        solver.solve();
+    }
+    return 0;
+}
+
