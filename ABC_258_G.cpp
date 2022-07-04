c8442daf4d9f976f5ed45c8c1710f9660a3b56c5
gtkn <sks.bumpts@gamil.com>
2022-07-04 23:10:47 +0900

    220704

---
+++ ABC_258_G.cpp
@@ -0,0 +1,101 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using P = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+#define dame { puts("-1"); return 0;}
+#define yn {puts("Yes");}else{puts("No");}
+
+//------------------------------------------------
+
+using bs = bitset<3000>;
+
+struct Solver{
+    struct edge{
+        ll to,c;
+        edge(ll to=0, ll c=0):to(to),c(c){}
+    };
+
+    struct abc{
+        ll a,b,c;
+        abc(ll a=0, ll b=0, ll c=0):a(a),b(b),c(c){}
+    };
+
+ 
+ 
+    vec(int) dh = {1,0,-1,0};
+    vec(int) dw = {0,1,0,-1};
+ 
+    void solve(){
+        ll N;
+        cin >> N;
+
+        vec(bs) A(N);
+        rep(i,N){
+            string ai; cin >> ai;
+            reverse(all(ai));
+            A[i] = bs(ai);
+        }
+        
+        ll ans = 0;
+        for(ll i=0;i<N;i++)for(ll j=i+1; j<N; j++){
+        //rep(i,N)rep(j,N){
+            if(!A[i].test(j)) continue;
+            ans += (A[i]&A[j]).count();
+            //cout << i << " , " << j << " ; " << (A[i]&A[j]).count() << endl; 
+        }
+        ans/=3;
+        cout << ans << endl;
+
+
+
+
+
+    }
+};
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        Solver solver;
+        solver.solve();
+    }
+    return 0;
+}
