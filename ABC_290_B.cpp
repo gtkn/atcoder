22899b4029a79c5551264ee8dbc87cf8e5a86216
gtkn <sks.bumpts@gamil.com>
2023-02-21 21:01:38 +0900

    230221

--- ABC_290_B.cpp
+++ ABC_290_B.cpp
@@ -21,6 +21,8 @@ using namespace std;
 
 #define all(x) x.begin(),x.end()
 #define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
 using ll = long long;
 using P = pair<int,int>;
 using Pll = pair<ll,ll>;
@@ -35,55 +37,47 @@ template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; }
 const ll llINF = 1LL << 60;
 const int iINF = 1e9;
 
+#define dame { puts("-1"); return;}
+#define yn {puts("Yes");}else{puts("No");}
+
 //------------------------------------------------
 
 struct Solver{
+    struct edge{
+        ll to,c;
+        edge(ll to=0, ll c=0):to(to),c(c){}
+    };
+
+    struct abc{
+        ll a,b,c;
+        abc(ll a=0, ll b=0, ll c=0):a(a),b(b),c(c){}
+    };
+
+ 
+ 
+    vec(int) dh = {1,0,-1,0};
+    vec(int) dw = {0,1,0,-1};
  
     void solve(){
-        ll N;
-        cin >> N;
-        vec(ll) A(N);
-        rep(i,N) cin >> A[i];
-        ll nn = -(N/2);
+        ll N,K;
+        cin >> N >> K;
 
-        vector<map<ll,ll>> dp(N);
-        rep(i,2){
-            dp[i][0]=0;
-            dp[i][-1]=A[i];
-        }
-        chmax(dp[1][-1], A[0]);
+        string S;
+        cin >> S;
 
-        ll th=10;
-        for(ll i=2;i<N;i++){
-            ll cnt=0;
-            for(auto mi:dp[i-1]){
-                dp[i][mi.first] = mi.second;
-                
-                cnt++;
-                if(cnt>th) break;
-            }
-            cnt=0;
-            for(auto mi:dp[i-2]){
-                ll j = mi.first-1;
-                if(j<nn) continue;
-
-                if(dp[i].find(j)==dp[i].end()) dp[i][j] = mi.second+A[i];
-                else chmax(dp[i][j], mi.second+A[i]);
-                
-                cnt++;
-                if(cnt>th) break;
+        rep(i,N){
+            if(S[i]=='o'){
+                if(K>0) K--;
+                else S[i]='x';
             }
         }
 
+        cout << S << endl;
 
-
-
-        ll ans = -llINF;
-        rep(i,N) if(dp[i].find(nn)!=dp[i].end()) chmax(ans,dp[i][nn]);
-        cout << ans << endl;
 
     }
 };
+
 
 
 int main(){
