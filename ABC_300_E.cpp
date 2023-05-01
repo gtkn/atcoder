5e8d7cbc5fb2ff6146e30c99980dd4a93d3b9ddd
gtkn <sks.bumpts@gamil.com>
2023-05-01 23:51:05 +0900

    230501

---
+++ ABC_300_E.cpp
@@ -0,0 +1,112 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+//using tri = tuple<ll,ll,ll>;
+using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+#define dame { puts("-1"); return;}
+#define yn {puts("Yes");}else{puts("No");}
+
+//------------------------------------------------
+
+struct Solver{
+    struct edge{
+        ll to,c;
+        edge(ll to=0, ll c=0):to(to),c(c){}
+    };
+
+    struct abc{
+        ll a,b,c;
+        abc(ll a=0, ll b=0, ll c=0):a(a),b(b),c(c){}
+    };
+
+ 
+ 
+    vec(int) dh = {1,0,-1,0};
+    vec(int) dw = {0,1,0,-1};
+ 
+    void solve(){
+        ll N;
+        cin >> N;
+
+        map<ll,mint> dp;
+        priority_queue<ll,vector<ll>,greater<ll>> q;
+
+        dp[1]=1;
+        q.push(1);
+
+        mint a = mint(5).inv();
+        map<ll,bool> used;
+
+        while(!q.empty()){
+            ll q0=q.top();
+            q.pop();
+            if(used[q0]) continue;
+            used[q0]=true;
+
+            for(ll x=2; x<=6; x++){
+                ll q1=q0*x;
+                if(q1>N) break;
+                dp[q1] += dp[q0]*a;
+                q.push(q1);
+            }
+        }
+
+        cout << dp[N].val() << endl;
+        // mint tmp=11;
+        // tmp/=125;
+        // cout << tmp.val() << endl;
+
+    
+
+
+    }
+};
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        Solver solver;
+        solver.solve();
+    }
+    return 0;
+}
