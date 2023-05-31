24907bea75edc1f71f29b3d1b82ce485a0f3231c
gtkn <sks.bumpts@gamil.com>
2023-05-31 22:16:37 +0900

    230531

--- ABC_303_F.cpp
+++ ABC_303_F.cpp
@@ -1,8 +1,8 @@
 //title
 #include <bits/stdc++.h>
 using namespace std;
-//#include <atcoder/all>
-//using namespace atcoder;
+#include <atcoder/all>
+using namespace atcoder;
 #define rep(i,n) for (ll i = 0; i < (n); ++i)
 #define rep1(i,n) for (ll i = 1; i <= (n); ++i)
 #define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
@@ -25,6 +25,7 @@ using namespace std;
 
 using ll = long long;
 using ld = long double;
+using l3 = __int128;
 
 using Pii = pair<int,int>;
 using Pll = pair<ll,ll>;
@@ -45,26 +46,100 @@ const int iINF = 1e9;
 
 //------------------------------------------------
 
+ll op(ll a, ll b){return max(a,b);}
+ll ee(){return 0LL;}
+
+
 struct Solver{
-    struct edge{
-        ll to,c;
-        edge(ll to=0, ll c=0):to(to),c(c){}
-    };
 
-    struct abc{
-        ll a,b,c;
-        abc(ll a=0, ll b=0, ll c=0):a(a),b(b),c(c){}
-    };
-
- 
- 
-    vec(int) dh = {1,0,-1,0};
-    vec(int) dw = {0,1,0,-1};
- 
     void solve(){
-        ll N;
-        cin >> N;
+        ll N, H;
+        cin >> N >> H;
 
+        vec(Pll) v(N);
+        rep(i,N) cin >> v[i].first >> v[i].second;
+
+        map<ll,vector<ll>> m;
+        rep(i,N) m[v[i].first].push_back(i);
+
+        vec(ll) va(N),vb(N); // vaが残りターンずっとのほう
+        rep(i,N) va[i] = v[i].second;
+
+        segtree<ll,op,ee> sega(va),segb(vb);
+
+
+        auto f = [&](ll d,ll a, ll t){
+            ll x = t*(t+1)/2 + a*t;
+            chmin(x, (H+d-1)/d);
+            return d*x;
+        };
+
+        ll ans = 0;
+        for(auto mi:m){
+            ll th = mi.first-ans;
+
+            ll da = sega.all_prod();
+            ll db = segb.all_prod();
+
+            ll tab = db/da - ans;
+            chmax(tab, 0LL);
+            chmin(tab, th);
+
+            // cout << "--now ;" <<ans <<" , " << th << ", " <<tab << " : " << H << endl;
+
+            ll tb = tab;
+            if(db>0 ) chmin(tb,  (H+db-1)/db );
+
+            H -= db*tb;
+            ans += tb;
+            if(H<=0) break;
+
+            // cout << tb << " , " << db << endl;
+
+            assert(tab==tb);
+
+            ll l=0,r=th-tab;
+            while(r-l>1){
+                ll mid = (l+r)/2;
+                // cout <<"        "<< l << " , " << r << " , " << mid <<"  , "<<da<<" , " << f(da,ans,mid) << endl;
+                if( f(da,ans,mid) < H) l=mid;
+                else r=mid;
+            }
+            ll ta = r;
+
+            // cout << ta << " , " << da << endl;
+
+
+            H -= f(da,ans,ta);
+            ans += ta;
+
+            if(H<=0) break;
+
+            assert(tb+ta==th);
+
+            for(ll i:mi.second){
+                sega.set(i, 0LL);
+                segb.set(i, v[i].first*v[i].second );
+            }
+
+        }
+
+
+        if(H>0){
+            ll db = segb.all_prod();
+            ll tb = (H+db-1)/db;
+
+            // cout << "--now ;" <<ans <<" , "  << H << endl;
+            // cout << db << " , " << tb << " , " << db*tb << endl;
+
+            H -= db*tb;
+            assert(H<=0);
+            ans += tb;
+        }
+
+
+
+        cout << ans << endl;
 
 
     }
