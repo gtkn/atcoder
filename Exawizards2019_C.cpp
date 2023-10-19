c6dbeff83d1bea9a57f5dadc9ed64160d48e4c68
gtkn <sks.bumpts@gamil.com>
2023-10-19 23:06:16 +0900

    231019

---
+++ Exawizards2019_C.cpp
@@ -0,0 +1,128 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define yn {puts("Yes");}else{puts("No");}
+
+//------------------------------------------------
+
+struct edge{
+    ll to,c;
+    edge(ll to=0, ll c=0):to(to),c(c){}
+};
+
+struct abc{
+    ll a,b,c;
+    abc(ll a=0, ll b=0, ll c=0):a(a),b(b),c(c){}
+};
+
+
+vec(ll) dh = {1,0,-1,0};
+vec(ll) dw = {0,1,0,-1};
+
+void solve(){
+    ll N,Q;
+    cin >> N >> Q;
+
+    string s;
+    cin >> s;
+
+    vec(char) t(Q),d(Q);
+    rep(i,Q) cin >> t[i] >> d[i];
+    vec(ll) dd(Q);
+    rep(i,Q) dd[i] = (d[i]=='L'? -1 : 1);
+
+
+    auto f = [&](ll x)->ll{
+        char c = s[x];
+        rep(i,Q){
+            if(c!=t[i]) continue;
+            x += dd[i];
+            if(x>=0 && x<N) c = s[x];
+            else c = '.';
+        }
+        return x;
+    };
+
+
+    ll ans = N;
+    ll l=0,r=N;
+    while(r-l>1){
+        ll mid = (l+r)/2;
+        if(f(mid)==-1) l=mid;
+        else r=mid;
+    }
+
+    // cout << l << " ";
+
+    if(f(0)==-1) ans -= r;
+
+    l=0; r=N;
+    while(r-l>1){
+        ll mid = (l+r)/2;
+        if(f(mid)==N) r=mid;
+        else l=mid;
+    }
+
+    // cout << l << " " << endl;
+
+    if(f(0)==N) ans = 0;
+    else ans -= (N-r);
+
+    cout << ans << endl;
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
