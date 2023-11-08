09187416565a3bea32e6cc7f51515d00cd5dcf3f
gtkn <sks.bumpts@gamil.com>
2023-11-08 23:09:17 +0900

    231108

---
+++ AGC_026_C.cpp
@@ -0,0 +1,100 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+using Pss = pair<string,string>;
+
+
+void solve(){
+    ll N;
+    cin >> N;
+
+    string S;
+    cin >> S;
+
+
+    map<Pss,ll> m0,m1;
+
+    auto f = [&](string ss, map<Pss,ll> &m){
+        rep(pt,(1<<N)){
+            string sa,sb;
+            ll t = pt;
+            rep(j,N){
+                if(t&1) sa += ss[j];
+                else sb += ss[j];
+                t>>=1;
+            }
+            m[{sa,sb}]++;
+        }
+    };
+
+    f(S.substr(0,N),m0);
+    reverse(all(S));
+    f(S.substr(0,N),m1);
+    
+    ll ans = 0;
+    for(auto mi:m0) ans += mi.second * m1[mi.first];
+
+    cout << ans << endl;
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
