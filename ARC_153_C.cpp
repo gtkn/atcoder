62647e013dbeba0025672f3e07b8f7e917631eec
gtkn <sks.bumpts@gamil.com>
2023-11-27 21:34:19 +0900

    231127

---
+++ ARC_153_C.cpp
@@ -0,0 +1,120 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+
+void solve(){
+    ll N;
+    cin >> N;
+
+    vec(ll) A(N);
+    rep(i,N) cin >> A[i];
+
+    vec(ll) B(N);
+
+    ll bb = 0;
+    repr(i,N){
+        bb += A[i];
+        B[i] = bb;
+    }
+
+    vec(ll) y(N);
+
+    if(B[0]!=0){
+        rep1(i,N-1) y[i] = abs(B[0]);
+        rep1(i,N-1) y[0] -= B[i]*abs(B[0]);
+        y[0]/=B[0];
+    }else{
+        ll X = 0;
+        rep1(i,N-1) X += B[i];
+        rep(i,N) y[i] = 1;
+
+        bool isok = false;
+        if(X>=0){
+            rep1(i,N-1)if(B[i]==-1){
+                y[i] += X;
+                isok = true;
+                break;
+            }
+        }else{
+            rep1(i,N-1)if(B[i]==1){
+                y[i] -= X;
+                isok = true;
+                break;
+            }
+        }
+        if(!isok) sayno;
+    }
+
+
+    vec(ll) ans(N);
+    ans[0] = y[0];
+    rep1(i,N-1) ans[i] = ans[i-1] + y[i];
+
+    cout << "Yes" << endl;
+    rep(i,N) cout << ans[i] << " "; cout << endl;
+
+
+
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
