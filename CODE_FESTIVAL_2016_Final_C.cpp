eb734931c4a3554a9af8a8e615b7b9afe0dc7c94
gtkn <sks.bumpts@gamil.com>
2023-11-28 21:28:35 +0900

    231128

---
+++ CODE_FESTIVAL_2016_Final_C.cpp
@@ -0,0 +1,97 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("YES");}else{puts("NO");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+
+
+void solve(){
+    ll N,M;
+    cin >> N >> M;
+
+    vec(ll) K(N);
+    vvec(ll) L(N);
+
+    rep(i,N){
+        cin >> K[i];
+        L[i].resize(K[i]);
+        rep(j,K[i]) cin >> L[i][j];
+    }
+
+    vvec(ll) vv(M+1);
+    rep(i,N){
+        for(ll l:L[i]) vv[l].push_back(i);
+    }
+
+    dsu d(N);
+
+    for(vec(ll) v:vv){
+        ll n = v.size();
+        rep(i,n-1) d.merge(v[i],v[i+1]);
+    }
+
+    if(d.groups()[0].size()==N) sayyn;
+    
+
+
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
