6578d44ac513f10bbd2c841e9c90bce323da4d77
gtkn <sks.bumpts@gamil.com>
2023-12-11 20:45:51 +0900

    231211

---
+++ ARC_106_D.cpp
@@ -0,0 +1,114 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+const ll llINF = 1LL << 60;
+const int iINF = 1e9;
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+
+//---modintで組み合わせ扱う用の構造体---
+struct mcomb{
+    ll nmax;
+    vec(mint) fa,af;
+    mcomb(ll sz=200020){
+        nmax = sz;
+        fa.resize(nmax+1);
+        fa[0]=1;
+        rep1(i,nmax) fa[i]=fa[i-1]*i;
+        af.resize(nmax+1);
+        rep(i,nmax+1) af[i]=fa[i].inv();
+    }
+    mint c(ll n, ll k){
+        if(n<k || k<0 || n>nmax) return 0;
+        return fa[n]*af[k]*af[n-k];
+    }
+};
+
+
+void solve(){
+    ll N,K;
+    cin >> N >> K;
+
+    vec(ll) A(N);
+    rep(i,N) cin >> A[i];
+
+    vec(mint) B(K+1);
+    vec(mint) ap(N,1);
+
+    B[0] = N;
+    rep1(k,K){
+        rep(i,N) ap[i] *= A[i];
+        rep(i,N) B[k] += ap[i];
+    }
+
+    mcomb mc(K+10);
+    mint two = 1;
+    rep1(x,K){
+        two *= 2;
+        mint res = 0;
+        rep(k,x+1) res += mc.c(x,k)*B[k]*B[x-k];
+        res -= two*B[x];
+        res/=2;
+
+        cout << res.val() << endl;
+    }
+
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
