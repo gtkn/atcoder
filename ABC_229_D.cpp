f64abf83be616b05f038630d076dcd1046d15dd3
gtkn <sks.bumpts@gamil.com>
2024-04-11 21:46:39 +0900

    240411

---
+++ ABC_229_D.cpp
@@ -0,0 +1,104 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+constexpr ll llINF = 1LL << 60;
+constexpr int iINF = 1e9;
+constexpr char nl = '\n';
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+
+void solve(){
+    string S;
+    cin >> S;
+    ll K;
+    cin >> K;
+
+    ll n = S.size();
+
+    fenwick_tree<ll> fw(n);
+    rep(i,n) if(S[i]=='.') fw.add(i,1);
+
+    if(fw.sum(0,n)<=K){
+        cout << n << endl;
+        return;
+    }
+
+    // rep(i,n) cerr << ( S[i]=='.' ? 1 : 0 ) << " "; cerr << endl;
+    // rep(i,n+1) cerr << fw.sum(0,i) << " "; cerr << endl;
+
+
+
+    ll ans = 0;
+    rep(i,n){
+        ll l=i, r=n+1;
+        while(r-l>1){
+            ll m = (l+r)/2;
+            if(fw.sum(i,m)<=K) l = m;
+            else r = m;
+        }
+        // if(fw.sum(i,n)<=K) l=n;
+        chmax(ans,l-i);
+    }
+
+    cout << ans << endl;
+
+
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
