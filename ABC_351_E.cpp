c253678fc0b44faf50a2e804b8af7784bad063e7
gtkn <sks.bumpts@gamil.com>
2024-04-28 22:07:45 +0900

    240428

---
+++ ABC_351_E.cpp
@@ -0,0 +1,119 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+constexpr ll llINF = 1LL << 60;
+constexpr int iINF = 1e9;
+constexpr char nl = '\n';
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+struct edge{
+    ll to,c,idx;
+    edge(ll to=0, ll c=0, ll idx=0):to(to),c(c),idx(idx){}
+};
+
+
+vec(ll) dh = {1,0,-1,0};
+vec(ll) dw = {0,1,0,-1};
+
+void solve(){
+    ll N;
+    cin >> N;
+
+    vec(Pll) xy(N);
+    rep(i,N) cin >> xy[i].first >> xy[i].second;
+
+    vec(Pll) po,pe;
+    rep(i,N){
+        if( (xy[i].first+xy[i].second)&1 ) po.push_back(xy[i]);
+        else pe.push_back(xy[i]);
+    }
+
+    for(auto &p:po) p.first--;
+
+    ll ans = 0;
+    
+    auto f = [&](const vec(Pll)& p)->void{
+        vec(ll) u,v;
+        for(auto [x,y]:p){
+            u.push_back( (x+y)/2 );
+            v.push_back( (x-y)/2 );
+        }
+
+        sort(all(u));
+        sort(all(v));
+
+        ll cum = 0;
+        rep(i,u.size()){
+            ans += i*u[i]-cum;
+            cum += u[i];
+        }
+        cum = 0;
+        rep(i,v.size()){
+            ans += i*v[i]-cum;
+            cum += v[i];
+        }
+    };
+
+    f(po);
+    f(pe);
+
+    cout << ans << endl;
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
