646f6acf54687cbab5e6da6f72cd1be06c0882e2
gtkn <sks.bumpts@gamil.com>
2024-05-22 21:22:15 +0900

    240522

---
+++ ABC_354_D.cpp
@@ -0,0 +1,109 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+constexpr ll llINF = 1LL << 60;
+constexpr int iINF = 1e9;
+constexpr char nl = '\n';
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+struct edge{
+    ll to,c,idx;
+    edge(ll to=0, ll c=0, ll idx=0):to(to),c(c),idx(idx){}
+};
+
+
+vec(ll) dh = {1,0,-1,0};
+vec(ll) dw = {0,1,0,-1};
+
+void solve(){
+    ll A,B,C,D;
+    cin >> A >> B >> C >> D;
+
+
+    vvec(ll) x(4);
+    x[0] = {2,1};
+    x[1] = {1,2};
+    x[2] = {0,1};
+    x[3] = {1,0};
+
+    vec(ll) cnt0(4),cnt1(2);
+    ll w = C-A;
+    rep(i,4) cnt0[i] = w/4;
+    ll s = mod(C-w%4,4);
+    rep(k,w%4) cnt0[(s+k)%4]++;
+
+    ll h = D-B;
+    rep(i,2) cnt1[i] = h/2;
+    ll t = mod(D-h%2,2);
+    rep(k,h%2) cnt1[(t+k)%2]++;
+
+    ll ans = 0;
+
+    // cerr << s << " , " << t << endl;
+    // rep(i,4) cerr << cnt0[i] << ' '; cerr << endl;
+    // rep(i,2) cerr << cnt1[i] << ' '; cerr << endl;
+
+    rep(i,4)rep(j,2) ans += cnt0[i]*cnt1[j]*x[i][j];
+
+    cout << ans << endl;
+
+    
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
