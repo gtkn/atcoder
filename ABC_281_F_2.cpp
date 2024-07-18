f39773e557741ba7db27cc3b18961b1051495b55
gtkn <sks.bumpts@gamil.com>
2024-07-18 21:57:18 +0900

    240718

---
+++ ABC_281_F_2.cpp
@@ -0,0 +1,110 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+//#include <atcoder/all>
+//using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+constexpr ll llINF = 1LL << 61;
+constexpr int iINF = 1e9;
+constexpr char nl = '\n';
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+struct edge{
+    ll to,c,idx;
+    edge(ll to=0, ll c=0, ll idx=0):to(to),c(c),idx(idx){}
+};
+
+
+vec(ll) dh = {1,0,-1,0};
+vec(ll) dw = {0,1,0,-1};
+
+void solve(){
+    ll N;
+    cin >> N;
+
+    vec(ll) a(N);
+    rep(i,N) cin >> a[i];
+
+    auto f = [](auto f, ll dep, const vec(ll)& v)->ll{
+        // cerr << dep << " " << v.size() << endl;
+        if(v.empty()) return llINF;
+        if(dep<0) return 0;
+        
+        vec(ll) v0,v1;
+        for(auto x:v){
+            if(bit(x,dep)==0) v0.push_back(x);
+            else v1.push_back(x);
+        }
+
+        ll t = (1<<dep);
+        ll res = llINF;
+        chmin(res,f(f,dep-1,v0) );
+        chmin(res,f(f,dep-1,v1) );
+
+        if(!v0.empty() && !v1.empty()) res += t;
+
+        // cerr << "---" << endl;
+        // cerr << dep << " " << res << endl;
+        // for(auto x:v) cerr << x << " "; cerr << endl;
+
+        return res;
+    };
+
+    ll ans = f(f,30,a);
+    cout << ans << endl;
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
