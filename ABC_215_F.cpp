eee5496a0393736f68fa7d0f62b3e7b7ace62cf4
gtkn <sks.bumpts@gamil.com>
2024-08-08 23:09:05 +0900

    240808

---
+++ ABC_215_F.cpp
@@ -0,0 +1,111 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+inline Pll PllSum(Pll x, Pll y){return {x.first+y.first,x.second+y.second};}
+constexpr ll llINF = 1LL << 61;
+constexpr int iINF = 1e9;
+constexpr char nl = '\n';
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+struct edge{
+    ll to,c,idx;
+    edge(ll to=0, ll c=0, ll idx=0):to(to),c(c),idx(idx){}
+};
+
+
+// vec(ll) dh = {1,0,-1,0};
+// vec(ll) dw = {0,1,0,-1};
+vec(Pll) dhw = { {1,0},{0,1},{-1,0},{0,-1} };
+
+void solve(){
+    ll N;
+    cin >> N;
+    // vec(ll) x(N),y(N);
+    // rep(i,N) cin >> x[i] >> y[i];
+    vec(Pll) xy(N);
+    rep(i,N) cin >> xy[i].first >> xy[i].second;
+
+    sort(all(xy));
+
+
+    auto f = [&](ll k)->bool{
+        ll ymin=llINF, ymax=-llINF;
+        ll ii = 0;
+        for(auto [x,y]:xy){
+            while(ii<N && xy[ii].first <= x-k){
+                chmin(ymin, xy[ii].second);
+                chmax(ymax, xy[ii].second);
+                ii++;
+            }
+            if(ymax-y>=k) return true;
+            if(y-ymin>=k) return true;
+        }
+        return false;
+    };
+
+    ll l=0,r=1e9+10;
+    while(r-l>1){
+        ll mid = (l+r)/2;
+        if(f(mid)) l=mid;
+        else r=mid;
+    }
+    cout << l << endl;
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
