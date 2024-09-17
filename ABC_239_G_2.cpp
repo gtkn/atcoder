a762773d7a27d2f819e21e500b4a4a964201eac4
gtkn <sks.bumpts@gamil.com>
2024-09-17 22:43:21 +0900

    240917

---
+++ ABC_239_G_2.cpp
@@ -0,0 +1,107 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+inline Pll PllSum(Pll x, Pll y){return {x.first+y.first,x.second+y.second};}
+constexpr ll llINF = 1LL << 61;
+constexpr int iINF = 1e9;
+constexpr char nl = '\n';
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+
+void solve(){
+    ll N,M;
+    cin >> N >> M;
+
+    ll st = 2*N, gl = 2*N+1;
+
+    mf_graph<ll> g(2*N+2);
+
+    rep(_,M){
+        ll u,v;
+        cin >> u >> v;
+        --u,--v;
+        g.add_edge(u+N,v,llINF);
+        g.add_edge(v+N,u,llINF);
+    }
+
+    rep(i,N){
+        ll c;
+        cin >> c;
+        if(c==0) c = llINF;
+        g.add_edge(i,i+N,c);
+    }
+
+    g.add_edge(st,0,llINF);
+    g.add_edge(N-1+N,gl,llINF);
+
+    ll C = g.flow(st,gl);
+
+    vec(ll) ans;
+
+    vec(bool) v = g.min_cut(st);    
+    rep(i,N){
+        if(v[i] && !v[i+N]) ans.push_back(i+1);
+    }
+
+    cout << C << endl;
+    cout << ans.size() << endl;
+    for(auto a:ans) cout << a << " "; cout << endl;
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    //cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
---
+++ ABC_240_F.cpp
@@ -0,0 +1,114 @@
+//title
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+#define rep1(i,n) for (ll i = 1; i <= (n); ++i)
+#define repr(i,n) for (ll i = (n)-1; i >= 0; --i)
+#define rep1r(i,n) for (ll i = (n); i > 0; --i)
+#define bit(n,k) ((n>>k)&1) //nのk bit目
+
+#define vec(T) vector<T>
+#define vvec(T) vector<vec(T)>
+#define vvvec(T) vector<vvec(T)>
+#define vvvvec(T) vector<vvvec(T)>
+
+//typedef vector<mint>vi;
+//typedef vector<vi>vvi;
+//typedef vector<vvi>vvvi;
+//typedef vector<vvvi>vvvvi;
+
+#define all(x) x.begin(),x.end()
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+#define sfind(s,x) (s.find(x)!=s.end())
+
+using ll = long long;
+using ld = long double;
+using l3 = __int128;
+
+using Pii = pair<int,int>;
+using Pll = pair<ll,ll>;
+using tri = tuple<ll,ll,ll>;
+// using tri = array<ll,3>;
+
+//using mint = modint1000000007;
+//using mint = modint998244353;
+
+
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+inline ll mod(ll a, ll m) {return (a % m + m) % m;}
+inline Pll PllSum(Pll x, Pll y){return {x.first+y.first,x.second+y.second};}
+constexpr ll llINF = 1LL << 61;
+constexpr int iINF = 1e9;
+constexpr char nl = '\n';
+
+#define dame { puts("-1"); return;}
+#define sayno { puts("No"); return;}
+#define sayyes { puts("Yes"); return;}
+#define sayyn {puts("Yes");}else{puts("No");}
+#define sayfs {puts("First");}else{puts("Second");}
+
+//------------------------------------------------
+
+
+void solve(){
+    ll N,M;
+    cin >> N >> M;
+    vec(ll) x(N),y(N);
+    rep(i,N) cin >> x[i] >> y[i];
+
+    ll ans = x[0];
+    ll b = 0, a = 0;
+
+    auto f = [](ll a0, ll d, ll n)->ll{
+        ll a1 = a0 + d;
+        ll a2 = a0 + n*d;
+        return (a1+a2)*n/2;
+    };
+
+
+    // cerr << "---" << nl;
+    rep(i,N){
+        ll xi = x[i], yi = y[i];
+
+        ll l = 0, r = yi+1;
+        while(r-l>1){
+            ll m = (l+r)/2;
+            ll bb = b + xi*m;
+            if(b * bb >= 0) l = m;
+            else r = m;
+        }
+
+        // cerr << "i: " << i << " xi: " << xi << " yi: " << yi << nl;
+        
+        a += f(b,xi,l);
+        b += xi*l;
+        chmax(ans,a);
+        // cerr << a << " , " << b << " , " << l << nl;
+
+        a += f(b,xi,yi-l);
+        b += xi*(yi-l);
+        chmax(ans,a);
+        // cerr << a << " , " << b << " , " << yi-l << nl;
+
+    }
+
+    cout << ans << nl;
+
+
+
+
+}
+
+
+
+int main(){
+    int testcasenum=1;
+    cin >> testcasenum;
+    rep1(ti,testcasenum){
+        solve();
+    }
+    return 0;
+}
